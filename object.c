d752655bc06f4a692344aa5525b9f2403d965be7
Lavanya <lavanyakanchinadham@gmail.com>
2026-04-21 13:44:36 +0000

    phase 1 : started object_read file reading

--- object.c
+++ object.c
@@ -178,6 +178,17 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 // Returns 0 on success, -1 on error (file not found, corrupt, etc.).
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
     // TODO: Implement
-    (void)id; (void)type_out; (void)data_out; (void)len_out;
-    return -1;
+   char path[512];
+   object_path(id, path, sizeof(path));
+
+   FILE *f = fopen(path, "rb");
+   if (!f) return -1;
+
+  fseek(f, 0, SEEK_END);
+  long size = ftell(f);
+  rewind(f);
+
+  char *buffer = malloc(size);
+  fread(buffer, 1, size, f);
+  fclose(f);
 }
