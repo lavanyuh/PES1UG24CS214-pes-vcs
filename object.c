34e28ac1a7d7dc63d8b52df806e1055d357e7744
Lavanya <lavanyakanchinadham@gmail.com>
2026-04-21 13:54:56 +0000

    phase 1 : fixed error in ojbect_read

--- object.c
+++ object.c
@@ -177,18 +177,64 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 // The caller is responsible for calling free(*data_out).
 // Returns 0 on success, -1 on error (file not found, corrupt, etc.).
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
-    // TODO: Implement
-   char path[512];
-   object_path(id, path, sizeof(path));
+    char path[512];
+    object_path(id, path, sizeof(path));
 
-   FILE *f = fopen(path, "rb");
-   if (!f) return -1;
+    FILE *f = fopen(path, "rb");
+    if (!f) return -1;
 
-  fseek(f, 0, SEEK_END);
-  long size = ftell(f);
-  rewind(f);
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    rewind(f);
 
-  char *buffer = malloc(size);
-  fread(buffer, 1, size, f);
-  fclose(f);
+    char *buffer = malloc(size);
+    if (!buffer) {
+        fclose(f);
+        return -1;
+    }
+
+    if (fread(buffer, 1, size, f) != (size_t)size) {
+      fclose(f);
+      free(buffer);
+      return -1;
+}
+    fclose(f);
+
+    ObjectID check;
+    compute_hash(buffer, size, &check);
+    if (memcmp(&check, id, sizeof(ObjectID)) != 0) {
+        free(buffer);
+        return -1;
+    }
+
+    char *null_pos = memchr(buffer, '\0', size);
+    if (!null_pos) {
+        free(buffer);
+        return -1;
+    }
+
+    size_t header_len = null_pos - buffer + 1;
+
+    if (strncmp(buffer, "blob", 4) == 0) *type_out = OBJ_BLOB;
+    else if (strncmp(buffer, "tree", 4) == 0) *type_out = OBJ_TREE;
+    else if (strncmp(buffer, "commit", 6) == 0) *type_out = OBJ_COMMIT;
+    else {
+        free(buffer);
+        return -1;
+    }
+
+    size_t data_len = size - header_len;
+    void *data = malloc(data_len);
+    if (!data) {
+        free(buffer);
+        return -1;
+    }
+
+    memcpy(data, buffer + header_len, data_len);
+
+    *data_out = data;
+    *len_out = data_len;
+
+    free(buffer);
+    return 0;
 }
